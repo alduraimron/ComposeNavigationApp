9689af2e95b0c24b7d8a7476668f78a3cddea5ea
Syifaniads <syfaads16@gmail.com>
2025-10-14 16:11:51 +0700

    Implement Step 3.3.1–3.3.2: HomeScreen and DetailScreen with navigation

--- app/src/main/java/com/example/composenavigationapp/ui/navigation/RootNavGraph.kt
+++ app/src/main/java/com/example/composenavigationapp/ui/navigation/RootNavGraph.kt
@@ -18,24 +18,28 @@ fun RootNavGraph(
     ) {
         // 1) Splash sebagai start
         composable(Routes.SPLASH) {
-            SplashScreen(
-                onFinished = {
-                    // Pindah ke main_graph dan hapus splash dari back stack
-                    navController.navigate(Routes.MAIN_GRAPH) {
-                        popUpTo(Routes.SPLASH) { inclusive = true }
-                        launchSingleTop = true
-                    }
+            SplashScreen(onFinished = {
+                // Pindah ke main_graph dan hapus splash dari back stack
+                navController.navigate(Routes.MAIN_GRAPH) {
+                    popUpTo(Routes.SPLASH) { inclusive = true }
+                    launchSingleTop = true
                 }
+            }
             )
         }
-        // 2) Main graph (akan diisi pada langkah berikut)
-        navigation(
-            startDestination = Routes.HOME,
-            route = Routes.MAIN_GRAPH
-        ) {
-            // Untuk sementara, definisikan minimal satu layar agar bisa dicoba
-            composable(Routes.HOME) { PlaceholderHomeScreen() }
-            // Nanti di Langkah 3+ kita tambah: DETAIL, PROFILE, SETTINGS, ADD
+        composable(Routes.MAIN_GRAPH) {
+            MainScaffold()
         }
     }
 }
+        // 2) Main graph (akan diisi pada langkah berikut)
+//        navigation(
+//            startDestination = Routes.HOME,
+//            route = Routes.MAIN_GRAPH
+//        ) {
+//            // Untuk sementara, definisikan minimal satu layar agar bisa dicoba
+//            composable(Routes.HOME) { PlaceholderHomeScreen() }
+//            // Nanti di Langkah 3+ kita tambah: DETAIL, PROFILE, SETTINGS, ADD
+//        }
+//    }
+//}
