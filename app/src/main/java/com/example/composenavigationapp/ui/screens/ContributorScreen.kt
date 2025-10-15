cb3d9a773d18c02d4b4f39d114024bd50ef925ee
Nukotani <fatihsatrio121@gmail.com>
2025-10-15 14:45:42 +0700

    Added an Icon

--- app/src/main/java/com/example/composenavigationapp/ui/screens/ContributorScreen.kt
+++ app/src/main/java/com/example/composenavigationapp/ui/screens/ContributorScreen.kt
@@ -3,11 +3,16 @@ package com.example.composenavigationapp.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Groups
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun NegroScreen() {
@@ -22,6 +27,11 @@ fun NegroScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        Icon(
+            imageVector = Icons.Filled.Groups,
+            contentDescription = "contributors",
+            modifier = Modifier.size(50.dp)
+        )
         contributors.forEach { it ->
             Text(
                 text = it.second,
