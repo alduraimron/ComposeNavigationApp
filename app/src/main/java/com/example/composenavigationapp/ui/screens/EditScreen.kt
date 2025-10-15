5587337d70948f228823b563add7fa6979b2ac62
Nukotani <fatihsatrio121@gmail.com>
2025-10-15 11:45:21 +0700

    created EditScreen.kt

---
+++ app/src/main/java/com/example/composenavigationapp/ui/screens/EditScreen.kt
@@ -0,0 +1,31 @@
+package com.example.composenavigationapp.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun EditScreen(navController: NavController, id: String?) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = { navController.popBackStack() }) {
+            Text("Kembali")
+        }
+    }
+}
